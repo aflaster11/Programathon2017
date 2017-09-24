e3b49c05d227cfa66fa36f60ed843d6a054c95ec
Ducan-Ally <Fakebian98@gmail.com>
2017-09-24 04:17:50 -0600

    Comparacion Tiempo

--- public/scripts/creacion_eventos.js
+++ public/scripts/creacion_eventos.js
@@ -6,12 +6,43 @@ var avances; //Bool??
 var valueToSave; //Bool??
 
 function crearEvento(){
-	nombreDelEvento = document.getElementById("inputDanger1");
-	fechaInicio = document.getElementById("inputEmail4");
-	fechaConclusion = document.getElementById("inputPassword4");
+	nombreDelEvento = document.getElementById("NombreEvento");
+	var primerFecha = document.getElementById("Inicio").value;
+	var segundaFecha = document.getElementById("Conclusion").value;
 	
-	if(nombreDelEvento == undefined || fechaInicio == undefined || fechaConclusion == undefined){
-		alert("Todos los espacios deben estar llenos");
+	console.log(primerFecha);
+	console.log(segundaFecha);
+	
+	if(nombreDelEvento == undefined){
+		alert("Falta Nombre");
+		return;
+	}
+	
+	if(primerFecha == undefined){
+		alert("Falta Inicio");
+		return;
+	}
+	
+	if(segundaFecha == undefined){
+		alert("Falta Conlcusion");
+		return;
+	}
+	
+	primerFecha = primerFecha.split("-");
+	segundaFecha = segundaFecha.split("-");
+	
+	fechaInicio = new Date(primerFecha[0] + "," + primerFecha[1] + "," + primerFecha[2] + "," + primerFecha[3] + "," + primerFecha[4])
+	fechaConclusion = new Date(segundaFecha[0] + "," + segundaFecha[1] + "," + segundaFecha[2] + "," + segundaFecha[3] + "," + segundaFecha[4]);
+	
+	for(var i=0;i < primerFecha.length-1;++i){
+		if(parseInt(primerFecha[i]) > parseInt(segundaFecha[i])){
+			alert("La fecha incio es mayor a la fecha conclusion");
+			return;
+		}
+	}
+	
+	if(parseInt(primerFecha[primerFecha.length-1]) == parseInt(segundaFecha[segundaFecha.length-1])){
+		alert("La fecha incio es mayor a la fecha conclusion");
 		return;
 	}
 	
