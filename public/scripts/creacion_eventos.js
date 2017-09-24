7df27124cc924cbc5a067142303715d159606066
Ducan-Ally <Fakebian98@gmail.com>
2017-09-24 05:52:08 -0600

    Cambio en crecion eventos

--- public/scripts/creacion_eventos.js
+++ public/scripts/creacion_eventos.js
@@ -13,42 +13,46 @@ function crearEvento(){
 	console.log(primerFecha);
 	console.log(segundaFecha);
 	
-	if(nombreDelEvento == undefined){
-		alert("Falta Nombre");
-		return;
-	}
-	
-	if(primerFecha == undefined){
-		alert("Falta Inicio");
-		return;
-	}
-	
-	if(segundaFecha == undefined){
-		alert("Falta Conlcusion");
+	if(nombreDelEvento == undefined || primerFecha == undefined || segundaFecha == undefined){
+		alert("Falta rellenar espacios");
 		return;
 	}
 	
 	primerFecha = primerFecha.split("-");
 	segundaFecha = segundaFecha.split("-");
 	
-	fechaInicio = new Date(primerFecha[0] + "," + primerFecha[1] + "," + primerFecha[2] + "," + primerFecha[3] + "," + primerFecha[4])
-	fechaConclusion = new Date(segundaFecha[0] + "," + segundaFecha[1] + "," + segundaFecha[2] + "," + segundaFecha[3] + "," + segundaFecha[4]);
-	
-	for(var i=0;i < primerFecha.length-1;++i){
-		if(parseInt(primerFecha[i]) > parseInt(segundaFecha[i])){
-			alert("La fecha incio es mayor a la fecha conclusion");
-			return;
-		}
-	}
-	
-	if(parseInt(primerFecha[primerFecha.length-1]) == parseInt(segundaFecha[segundaFecha.length-1])){
-		alert("La fecha incio es mayor a la fecha conclusion");
+	if(!cumpleReq(primerFecha,segundaFecha)){
 		return;
 	}
+	
+	fechaInicio = new Date(primerFecha[0] + "," + primerFecha[1] + "," + primerFecha[2] + "," + primerFecha[3] + "," + primerFecha[4])
+	fechaConclusion = new Date(segundaFecha[0] + "," + segundaFecha[1] + "," + segundaFecha[2] + "," + segundaFecha[3] + "," + segundaFecha[4]);
 	
 	tipoDeVoto = $("input[name=optionsRadios1]:checked").val();
 	avances = $("input[name=optionsRadios2]:checked").val();
 	valueToSave = $("input[name=optionsRadios3]:checked").val();
 	
 	console.log(tipoDeVoto + " " + avances + " " + valueToSave);
+}
+
+function cumpleReq(pFecha,sFecha){
+	var cumple = true;
+	
+	console.log("h");
+	for(var i=0;i < pFecha.length-1 || !cumple;++i){
+		if(parseInt(pFecha[i]) > parseInt(sFecha[i])){
+			cumple = false;
+		}
+	}
+	
+	console.log("h");
+	if(parseInt(pFecha[pFecha.length-1]) >= parseInt(sFecha[sFecha.length-1]) || !cumple){
+		alert("La hora inicio debe ser menor a la hora de finalizacion");
+	}
+	
+	var currentDate = new Date();
+	console.log(currentDate.getMinutes());
+	
+	console.log("h");
+	return cumple;
 }
