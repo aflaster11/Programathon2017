1ce3ebd59e9ee46f6894502726f44bf91d87d4a8
aflaster11 <a.flaster@gmail.com>
2017-09-23 20:06:45 -0600

    votar

--- Contract/YoElijo.sol
+++ Contract/YoElijo.sol
@@ -3,16 +3,16 @@ import './VotosToken.sol';
 
 contract owned{
     address owner;
-    
+
     modifier onlyOwner{
         require(msg.sender == owner);
         _;
     }
-    
+
     function owned() public{
         owner = msg.sender;
     }
-    
+
     function transferOwnership(address _newOwner) public onlyOwner{
         owner = _newOwner;
     }
@@ -20,7 +20,8 @@ contract owned{
 
 contract YoElijo is owned{
     VotosToken public votosToken = new VotosToken();
-    
+    mapping(address => bool) public yaVoto;
+
     function repartirTokens(address[] votantes, uint max) public{
         uint votos = votantes.length * max;
         for(uint i = 0; i < votantes.length; ++i){
@@ -28,4 +29,16 @@ contract YoElijo is owned{
             votos -= max;
         }
     }
+}
+
+modifier yaVoto(address _votante){
+  require(yaVoto[_votante] == false);
+  _;
+}
+
+function votar(address _votante, address[] _opcion) yaVoto{
+  for(int i = 0; i < _opcion.length-1;++i){
+    opciones[_opcion[i]]++;
+  }
+  yaVoto[_votante] = true;
 }
