d783ef7ef5203a3488908116258fc118196f161d
Ducan-Ally <Fakebian98@gmail.com>
2017-09-24 02:41:51 -0600

    Fotosintesis

--- public/scripts/creacion_eventos.js
+++ public/scripts/creacion_eventos.js
@@ -10,6 +10,11 @@ function crearEvento(){
 	fechaInicio = document.getElementById("inputEmail4");
 	fechaConclusion = document.getElementById("inputPassword4");
 	
+	if(nombreDelEvento == undefined || fechaInicio == undefined || fechaConclusion == undefined){
+		alert("Todos los espacios deben estar llenos");
+		return;
+	}
+	
 	tipoDeVoto = $("input[name=optionsRadios1]:checked").val();
 	avances = $("input[name=optionsRadios2]:checked").val();
 	valueToSave = $("input[name=optionsRadios3]:checked").val();
