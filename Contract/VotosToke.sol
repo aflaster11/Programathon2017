1c9d662982d0c129253d5ac87202d0467732ee71
aflaster11 <a.flaster@gmail.com>
2017-09-23 20:15:12 -0600

    Cambios x

--- Contract/VotosToke.sol
+++ Contract/VotosToke.sol
@@ -4,22 +4,22 @@ contract VotosToken{
     mapping(address => uint256) public votosPer;
     mapping(address => uint256) public opciones;
     mapping(address => address[]) public misVotos;
-    
+
     function Balance(address votante) public constant returns (uint256){
         require(votosPer[votante] > 0);
         return votosPer[votante];
     }
-    
+
     function Transferir(address votante,uint256 votos) public{
         require(votos > 0);
         votosPer[votante] += votos;
     }
-    
+
     function TransferirDe(address votante, address opcion, uint256 votos) public{
         require(votos == 1);
         require(votosPer[votante] > 0 && opciones[votante] == 0);
         require(votosPer[opcion]==0);
-        
+
         votosPer[votante] -= votos;
         opciones[opcion] += votos;
     }
