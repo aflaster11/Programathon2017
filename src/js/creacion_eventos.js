f499f4c50a7b51dcd2d6109c304f0d6d6370007a
Ducan-Ally <Fakebian98@gmail.com>
2017-09-23 23:11:38 -0600

    Cambios creacion-Eventos

---
+++ src/js/creacion_eventos.js
@@ -0,0 +1,26 @@
+var nombreDelEvento; //String
+var fechaInicio; //Int??
+var fechaConclusion; //Int??
+var tipoDeVoto; //Bool>>
+var avances; //Bool??
+var valueToSave; //Bool??
+
+function crearEvento(){
+	nombreDelEvento = document.getElementById("inputDanger1");
+	fechaInicio = document.getElementById("inputEmail4");
+	fechaConclusion = document.getElementById("inputPassword4");
+	
+	tipoDeVoto = $("input[name=optionsRadios1]:checked").val();
+	avances = $("input[name=optionsRadios2]:checked").val();
+	valueToSave = $("input[name=optionsRadios3]:checked").val();
+	
+	if(!valueToSave){
+		alert("Seleccione si quiere o no avance");
+	}
+	
+	if
+}
+
+function fechasCorrectas(){
+	
+}
